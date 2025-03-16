b2737b7429304d526aaf197eaa20a280401df095
MarioElguera <marioelguera63@alumnos.cei.es>
2025-03-16 16:29:45 +0100

    refactor code - ida 13

--- dia13/ejercicios/src/app/page.js
+++ dia13/ejercicios/src/app/page.js
@@ -9,6 +9,9 @@ export default function Home() {
     'https://sigdeletras.com/images/blog/202004_react_leaflet/react.png',
     'https://media2.dev.to/dynamic/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fi%2Fs15ubgod56c7butyt7eu.jpg',
     'https://cloudappi.net/wp-content/uploads/2024/08/Diseno-sin-titulo-5.png',
+    'https://www.matridtech.net/wp-content/uploads/2018/01/Features-of-AngularJS.png',
+    "https://es.mypet.com/wp-content/uploads/sites/23/2021/03/GettyImages-623368750-e1582816063521-1.jpg",
+    "https://es.onlyfresh.com/cdn/shop/articles/AManova_diarrea_nei_gatti.jpg",
   ];
 
   return (
@@ -19,7 +22,7 @@ export default function Home() {
       <hr></hr>
       <br></br>
       <h1>Carousel</h1>
-      <Carousel images={images} />
+      <Carousel images={images} visibleSlides={3} />
 
     </div>
 
--- dia13/ejercicios/src/components/ej2.js
+++ dia13/ejercicios/src/components/ej2.js
@@ -1,104 +1,70 @@
-import { useState, useEffect, useRef } from "react";
+"use client";
+import { useEffect, useState } from "react";
+import "./Carousel.css";
 
-const Carousel = ({ images, visibleSlides = 3 }) => {
+export default function Carousel({ images, visibleSlides = 3 }) {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const maxIndex = images.length;
-    const autoplayRef = useRef(null);
-    const transitionDuration = 500;
-    const autoPlayInterval = 3000;
+    const [isTransitioning, setIsTransitioning] = useState(true);
+    const maxIndex = images.length - visibleSlides;
 
-    useEffect(() => {
-        const startAutoplay = () => {
-            autoplayRef.current = setInterval(() => {
-                nextSlide();
-            }, autoPlayInterval);
-        };
-
-        startAutoplay();
-
-        return () => {
-            if (autoplayRef.current) {
-                clearInterval(autoplayRef.current);
-            }
-        };
-    }, [currentIndex]);
 
     const nextSlide = () => {
-        setCurrentIndex((prevIndex) => {
-            if (prevIndex === maxIndex - 1) {
-                return 0;
-            }
-            return prevIndex + 1;
-        });
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => {
-            if (prevIndex === 0) {
-                return maxIndex - 1;
-            }
-            return prevIndex - 1;
-        });
-    };
-
-    const handleTouchStart = (e) => {
-        setStartX(e.touches[0].clientX);
-    };
-
-    const handleTouchMove = (e) => {
-        setEndX(e.touches[0].clientX);
-    };
-
-    const handleTouchEnd = () => {
-        if (startX - endX > 100) {
-            nextSlide();
-        } else if (endX - startX > 100) {
-            prevSlide();
+        if (currentIndex === maxIndex) {
+            setIsTransitioning(false);
+            setTimeout(() => {
+                setCurrentIndex(0);
+                setTimeout(() => setIsTransitioning(true), 50);
+            }, 0);
+        } else {
+            setCurrentIndex(currentIndex + 1);
         }
     };
 
-    const angle = 360 / visibleSlides;
-
-    const getSlidePosition = () => {
-        return (currentIndex * (100 / visibleSlides));
+    const prevSlide = () => {
+        if (currentIndex === 0) {
+            setIsTransitioning(false);
+            setTimeout(() => {
+                setCurrentIndex(maxIndex);
+                setTimeout(() => setIsTransitioning(true), 50);
+            }, 0);
+        } else {
+            setCurrentIndex(currentIndex - 1);
+        }
     };
 
     return (
-        <div className="relative w-full max-w-3xl mx-auto overflow-hidden">
+        <div className="carousel-container">
             <div
-                className="flex transition-transform ease-in-out"
+                className="carousel-track"
                 style={{
-                    transform: `rotateY(${currentIndex * angle}deg) translateZ(250px)`,
-                    transitionDuration: `${transitionDuration}ms`,
+                    transform: `translateX(-${currentIndex * (100 / visibleSlides)}%)`,
+                    transition: isTransitioning ? "transform 0.5s ease-in-out" : "none",
                 }}
-                onTouchStart={handleTouchStart}
-                onTouchMove={handleTouchMove}
-                onTouchEnd={handleTouchEnd}
             >
                 {images.map((img, index) => (
                     <img
                         key={index}
                         src={img}
                         alt={`Slide ${index + 1}`}
-                        className="w-[calc(100%/3)] h-[200px] object-cover"
+                        className="carousel-image"
+                        style={{ width: `calc(100% / ${visibleSlides})` }}
                     />
                 ))}
             </div>
 
             <button
                 onClick={prevSlide}
-                className="absolute top-1/2 left-2 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black/70"
+                className="carousel-button carousel-button-prev"
             >
-                ←
+                {"<"}
             </button>
+
             <button
                 onClick={nextSlide}
-                className="absolute top-1/2 right-2 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black/70"
+                className="carousel-button carousel-button-next"
             >
-                →
+                {">"}
             </button>
         </div>
     );
-};
-
-export default Carousel;
+}
